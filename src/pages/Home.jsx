cf6ae7045d47b078242b5e456b944e4e38802623
Maks <mmaksimov20000806@gmail.com>
2024-09-18 20:04:50 +0200

    changed structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import Countries from "../components/Countries.jsx"
 
 export default function HomePage() {
 	return (
-		<main className='main flex flex-col mx-[4rem]'>
+		<section className='flex flex-col mx-[4rem]'>
 			<InputField />
 			<Countries />
-		</main>
+		</section>
 	)
 }
 
